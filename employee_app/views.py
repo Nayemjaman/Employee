fbd3b4753468c96cb49c81f5b30574a0624d9493
Nayem Jaman Tusher <njaman26@gmail.com>
2022-11-19 03:40:52 +0600

    views all employee completed

--- employee_app/views.py
+++ employee_app/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from .models import Employee,Depertment,Role
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 def all_emp(request):
-    return render(request, "all_emp.html")
+    all_employee = Employee.objects.all()
+    context = {
+        'all_employee': all_employee
+    }
+    return render(request, "all_emp.html",context)
 def add_emp(request):
     return render(request, 'add_emp.html')
 

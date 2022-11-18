b311cfc4daa9bf84dd3a3524ed19ef972d2505ec
Nayem Jaman Tusher <njaman26@gmail.com>
2022-11-19 03:09:37 +0600

    initilaze views

--- employee_app/urls.py
+++ employee_app/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('all_emp', views.all_emp, name='all_emp'),
+    path('add_emp', views.add_emp, name='add_emp'),
+    path('remove_emp', views.remove_emp, name='remove_emp'),
+    path('filter_emp', views.filter_emp, name='filter_emp')
 ]
--- employee_app/views.py
+++ employee_app/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+def all_emp(request):
+    return render(request, "all_emp.html")
+def add_emp(request):
+    return render(request, 'add_emp.html')
+
+def remove_emp(request):
+    return render(request, 'remove_emp.html')
+    
+def filter_emp(request):
+    return render(request, 'filter_emp.html')

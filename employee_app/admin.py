aed5e6bf17de78c70f3b972ccc7d05a02240f237
Nayem Jaman Tusher <njaman26@gmail.com>
2022-11-19 02:29:04 +0600

    models done and start bootstrap templates

---
+++ employee_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from  .models import Depertment,Role,Employee
+
+# Register your models here.
+
+admin.site.register(Depertment)
+admin.site.register(Role)
+admin.site.register(Employee)
+
---
+++ employee_app/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+class Depertment(models.Model):
+    name = models.CharField(max_length=100,null=False)
+    location = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=100,null=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100,null=False)
+    last_name = models.CharField(max_length=100,null=False)
+    salary = models.IntegerField(null=False,default=0)
+    bouns = models.IntegerField(null=False,)
+    phone_number = models.CharField(max_length=100,null=False)
+    hire_date = models.DateField()
+
+    dept = models.ForeignKey(Depertment,on_delete=models.CASCADE)
+    role = models.ForeignKey(Role,on_delete=models.CASCADE)
+
+    
+    def __str__(self) -> str:
+        return '%s %s' %(self.first_name,self.last_name)
